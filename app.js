7d9a45c9c9ac3d1e9f33e8c05272ef0b43056de2
Mohd Sami <msami9503@gmail.com>
2022-02-22 14:54:41 +0530

    server

---
+++ app.js
@@ -0,0 +1,36 @@
+let express = require("express");
+
+let data = require("./data.json");
+
+//ek naya server bnade but ye sirf create krti use chalu nhi krti
+let server = express();
+
+server.get("/movies", function (req, res) {
+  res.json(data);
+});
+
+server.get("/genre", function (req, res) {
+  let allGenreObjects = data.map(function (el) {
+    return el.genre;
+  });
+
+  let uniqueGenreObjects = [];
+
+  for (let i = 0; i < allGenreObjects.length; i++) {
+    let genreId = allGenreObjects[i]["_id"];
+
+    let index = uniqueGenreObjects.findIndex(function (el) {
+      return el._id == genreId;
+    });
+
+    if (index == -1) {
+      uniqueGenreObjects.push(allGenreObjects[i]);
+    }
+  }
+
+  res.json(uniqueGenreObjects);
+});
+
+ 
+server.listen(process.env.PORT ||4000);
+ 
